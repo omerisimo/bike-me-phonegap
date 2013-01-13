a47514573865cd7529b2b1cbf997ef0279d60b33
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2013-01-13 20:11:35 +0200

    Add a method to set default JQuery Mobile settings.

--- assets/www/javascript/bike_me/app.js
+++ assets/www/javascript/bike_me/app.js
@@ -4,10 +4,18 @@ function createNamespace (namespace, context) {
 
 bikeMe = {
   initialize: function () {
+    this.setJqueryMobileDefaults();
+
     this.searchView = new bikeMe.Views.Search();
+    this.mapView    = new bikeMe.Views.Map();
   },
 
   namespace: function (namespace) {
     return _.reduce(namespace.split('.'), createNamespace, this);
+  },
+
+  setJqueryMobileDefaults: function () {
+    $.mobile.defaultPageTransition = 'none';
   }
 };
+
