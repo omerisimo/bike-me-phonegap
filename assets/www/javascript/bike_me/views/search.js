c28c91e85ece9cba465584dccad09d68f3e9920e
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2013-01-13 20:11:10 +0200

    Implement Map View and Search View.

--- assets/www/javascript/bike_me/views/search.js
+++ assets/www/javascript/bike_me/views/search.js
@@ -6,34 +6,20 @@ bikeMe.Views.Search = function () {
 
 bikeMe.Views.Search.prototype = {
   initialize: function () {
-    this.$el   = $('#search');
-    this.$from = this.$el.find('input#from');
-    this.$to   = this.$el.find('input#to');
-
-    radio('onSearchSuccess').subscribe([this.onSearchSuccess, this]);
-    radio('onSearchFailure').subscribe([this.onSearchFailure, this]);
+    this.$el = $('#search');
 
     this.search = _.bind(this.search, this);
     this.$el.on('click', 'input[type="submit"]', this.search);
   },
 
   search: function () {
-    var from = this.$from.val();
-    var to   = this.$to.val();
+    var from = this.$el.find('input#from').val();
+    var to   = this.$el.find('input#to').val();
 
-    //router.search(from, to);
-    var routes = JSON.parse(window.localStorage.getItem("routes3")) || [];
-    routes.push({from: from, to: to});
-    window.localStorage.setItem("routes3", JSON.stringify(routes));
-    alert(routes.length);
-    //radio('onSearchSuccess').broadcast();
+    //bikeMe.Models.RouteFinder(from, to);
   },
 
-  onSearchSuccess: function () {
-    alert('Success');
-  },
-
-  onSearchFailure: function () {
-    alert('Failure');
+  unbind: function () {
+    this.$el.unbind();
   }
 };
