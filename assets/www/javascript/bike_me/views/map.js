c98171674f6d22336f690c801bbd0fdb588bf2c5
Omer Gazit <Omer.Gazit@Klarna.com>
2013-01-17 21:59:32 +0200

    Render route markers on the map

--- assets/www/javascript/bike_me/views/map.js
+++ assets/www/javascript/bike_me/views/map.js
@@ -16,13 +16,37 @@ bikeMe.Views.Map.prototype = {
     if (_.isUndefined(this.googleMap)){
       this.googleMap = new google.maps.Map(this.$googleMap[0], this.options);
       this.googleDirectionsService = new google.maps.DirectionsService();
-      this.directionsRenderer = new google.maps.DirectionsRenderer();
+      var rendererOptions = { suppressMarkers: true };
+      this.directionsRenderer = new google.maps.DirectionsRenderer(rendererOptions);
       this.directionsRenderer.setMap(this.googleMap);
+
+      this.stationIcons = { avaliableStation:         new google.maps.MarkerImage("images/path_bike_spot_icon_green.png",
+                                                                new google.maps.Size(25, 39),
+                                                                new google.maps.Point(0,0),
+                                                                new google.maps.Point(13, 38)),
+                           partiallyAvailableStation: new google.maps.MarkerImage("images/path_bike_spot_icon_orange.png",
+                                                                                    new google.maps.Size(25, 39),
+                                                                                    new google.maps.Point(0,0),
+                                                                                    new google.maps.Point(13, 38)),
+                           noneAvailableStation:      new google.maps.MarkerImage("images/path_bike_spot_icon_red.png",
+                                                                                    new google.maps.Size(25, 39),
+                                                                                    new google.maps.Point(0,0),
+                                                                                    new google.maps.Point(13, 38))
+                          };
+
+      this.originIcon = new google.maps.MarkerImage("images/path_green_arrow_icon.png",
+                                                      new google.maps.Size(32, 42),
+                                                      new google.maps.Point(0,0),
+                                                      new google.maps.Point(16, 34));
+      this.destinationIcon = new google.maps.MarkerImage("images/path_flag_icon.png",
+                                                          new google.maps.Size(32, 42),
+                                                          new google.maps.Point(0,0),
+                                                          new google.maps.Point(4, 43));
     }
   },
 
   options: {
-    center           : new google.maps.LatLng(32.0833, 34.8000),
+    center           : new google.maps.LatLng(-34.397, 150.644),
     disableDefaultUI : true,
     mapTypeId        : google.maps.MapTypeId.ROADMAP,
     zoom             : 8
@@ -41,24 +65,57 @@ bikeMe.Views.Map.prototype = {
   renderRoute: function (route) {
     var start = new google.maps.LatLng(route.source.latitude, route.source.longitude);
     var end   = new google.maps.LatLng(route.target.latitude, route.target.longitude);
+    var startStation = new google.maps.LatLng(route.sourceStation.location.latitude, route.sourceStation.location.longitude);
+    var endStation = new google.maps.LatLng(route.targetStation.location.latitude, route.targetStation.location.longitude);
 
-    var waypts = [{ location:new google.maps.LatLng(route.sourceStation.location.latitude, route.sourceStation.location.longitude), stopover:true },
-      { location:new google.maps.LatLng(route.targetStation.location.latitude, route.targetStation.location.longitude), stopover:true }
+    var waypts = [{ location: startStation, stopover: true },
+      { location: endStation, stopover: true }
     ];
 
     var request = { origin:start,
       destination:end,
       waypoints: waypts,
-      optimizeWaypoints: true,
+      optimizeWaypoints: false,
       travelMode: google.maps.TravelMode.WALKING
     };
 
-    var renderer = this.directionsRenderer;
-
-    this.googleDirectionsService.route(request, function(result, status) {
+    var succ = function(result, status) {
       if (status == google.maps.DirectionsStatus.OK) {
-        renderer.setDirections(result);
+        this.directionsRenderer.setDirections(result);
+        this.renderMarkers(route,
+                            result.routes[0].legs[0].start_location,
+                            result.routes[0].legs[2].end_location,
+                            result.routes[0].legs[0].end_location,
+                            result.routes[0].legs[2].start_location);
       }
-    });
+    }
+
+    var succ = _.bind(succ, this);
+
+    this.googleDirectionsService.route(request, succ);
+  },
+
+  renderMarkers: function (route, start, end, startStation, endStation){
+    var marker = new google.maps.Marker({position: start, title:"Origin", icon: this.originIcon});
+    marker.setMap(this.googleMap);
+
+    marker = new google.maps.Marker({position: end, title:"Destiantion", icon: this.destinationIcon});
+    marker.setMap(this.googleMap);
+
+    marker = new google.maps.Marker({position: startStation, title:"Origin Station", icon: this.getStationIcon(route.sourceStation.availableBikes)});
+    marker.setMap(this.googleMap);
+
+    marker = new google.maps.Marker({position: endStation, title:"Destiantion Station", icon: this.getStationIcon(route.targetStation.availableDocks)});
+    marker.setMap(this.googleMap);
+  },
+
+  getStationIcon: function (availableCount) {
+    if (availableCount > 2) {
+      return this.stationIcons.avaliableStation;
+    } else if (availableCount > 0) {
+      return this.stationIcons.partiallyAvailableStation;
+    } else {
+      return this.stationIcons.noneAvailableStation;
+    }
   }
 };
