5d9a6e524f5fddfe8f3ad6965d6932f16ae428c2
Omer Gazit <Omer.Gazit@Klarna.com>
2014-08-17 20:10:12 +0300

    Fix splash screen crash + cleanup of #onDeviceReady

--- assets/www/javascript/bike_me/app.js
+++ assets/www/javascript/bike_me/app.js
@@ -22,20 +22,9 @@ bikeMe = {
   },
 
   onDeviceReady: function() {
-    navigator.splashscreen.hide();
-    var networkState = navigator.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        
-        networkState = navigator.network.connection.type
+    if(navigator.splashscreen) {
+      navigator.splashscreen.hide();
+    }
   },
 
   alert: function (message, title) {
